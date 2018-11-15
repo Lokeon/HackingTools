66aa66043dbb3f154455bc72172d9da800e1832f
Lokeon <kevin.lopezcala99@gmail.com>
2018-11-15 15:30:53 +0100

    :penguin:

--- mac_changers/mac_changerwA.py
+++ mac_changers/mac_changerwA.py
@@ -7,7 +7,12 @@ def get_arguments():
     parser = optparse.OptionParser()
     parser.add_option("-i","--interface", dest="interface", help="Interface to change MAC address")
     parser.add_option("-m","--mac", dest="new_mac", help="New MAC address")
-    return parser.parse_args()
+    (options, arguments) = parser.parse_args()
+    if not options.interface:
+        parser.error("[-] Please specify an interface , use -- help for more info.")
+    elif not options.new_mac:
+        parser.error("[-] Please specify a new mac , use -- help for more info.")
+    return options
 
 def change_mac(interface,new_mac):
     # Put [ ] for security
@@ -15,7 +20,7 @@ def change_mac(interface,new_mac):
     subprocess.call(["ifconfig", interface, "hw", "ether", new_mac ])
     subprocess.call(["ifconfig", interface, "up"])
 
-(options, arguments) = get_arguments()
-change_mac(options.interface,options.new_mac)
+ops = get_arguments()
+change_mac(ops.interface,ops.new_mac)
 print("Changed")
  
