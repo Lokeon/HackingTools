5288ba86b589656792450c8d3d7af7eec6a2d516
Lokeon <kevin.lopezcala99@gmail.com>
2018-11-15 16:08:57 +0100

    :penguin:

--- mac_changers/mc_wArgs.py
+++ mac_changers/mc_wArgs.py
@@ -2,6 +2,7 @@
 
 import subprocess
 import optparse
+import re
 
 def get_arguments():
     parser = optparse.OptionParser()
@@ -20,7 +21,29 @@ def change_mac(interface,new_mac):
     subprocess.call(["ifconfig", interface, "hw", "ether", new_mac ])
     subprocess.call(["ifconfig", interface, "up"])
 
+def get_current_mac(interface):
+    ifconf_res = subprocess.check_output(["ifconfig", interface])
+    # rule to found MAC on ifconfig
+    mac_address_res = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w",ifconf_res)
+    if mac_address_res:
+        return mac_address_res.group(0)
+    else:
+        print("[-] Could not read MAC address.")
+
+def compr_mac(current_mac,new_mac):
+    if current_mac == new_mac:
+        #casting for none result 
+        print("[+] MAC Address changed to "+ str(current_mac))
+    else:
+        print("[-] MAC address didn't get changed")
+
 ops = get_arguments()
+current_mac = get_current_mac(ops.interface)
+#casting for none result 
+print("Current MAC = "+ str(current_mac))
+
 change_mac(ops.interface,ops.new_mac)
-print("Changed")
- 
+print("[+] Changing ")
+
+current_mac = get_current_mac(ops.interface)
+compr_mac(current_mac,ops.new_mac)
