497585697dc207e5084b3ff325568b4184e7df30
Lokeon <kevin.lopezcala99@gmail.com>
2018-11-15 14:17:35 +0100

    :penguin:

---
+++ mac_changer.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python
+
+import subprocess
+
+interface = raw_input()
+
+subprocess.call("ifconfig", Shell=True)
+subprocess.call("ifconfig", Shell=True)
+subprocess.call("ifconfig", Shell=True)
+ 
+ 
